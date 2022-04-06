3f62e075e30dff8353b50899a592313105bbdab2
Jan Kaul <jankaul@mailbox.org>
2022-04-06 10:59:30 +0200

    scanexec

--- src/physical_plan/mod.rs
+++ src/physical_plan/mod.rs
@@ -1,11 +1,59 @@
-use arrow2::datatypes::Schema;
+use arrow2::datatypes::{Field, Schema};
 
-use crate::record_batch::RecordBatch;
+use crate::{data_source::DataSource, record_batch::RecordBatch};
 
 pub mod physical_expressions;
 
-pub trait PhysicalPlan {
-    fn schema(&self) -> &Schema;
-    fn execute(self) -> Vec<RecordBatch>;
-    fn children(&self) -> Option<&[Box<dyn PhysicalPlan>]>;
+pub enum PhysicalPlan {
+    Scan(ScanExec),
+}
+
+impl PhysicalPlan {
+    pub fn schema(&self) -> &Schema {
+        match self {
+            PhysicalPlan::Scan(scan) => scan.schema(),
+        }
+    }
+}
+
+pub struct ScanExec {
+    data_source: DataSource,
+    projection: Option<Vec<String>>,
+    schema: Schema,
+}
+
+impl ScanExec {
+    pub fn new(data_source: DataSource, projection: Option<Vec<String>>) -> Self {
+        ScanExec {
+            schema: Self::derive_schema(&data_source, &projection),
+            data_source: data_source,
+            projection: projection,
+        }
+    }
+
+    fn derive_schema(data_source: &DataSource, projection: &Option<Vec<String>>) -> Schema {
+        match projection {
+            Some(pro) => data_source
+                .schema()
+                .fields
+                .iter()
+                .filter(|x| pro.contains(&x.name))
+                .map(|y| y.clone())
+                .collect::<Vec<Field>>()
+                .into(),
+            None => data_source.schema(),
+        }
+    }
+}
+
+impl ScanExec {
+    fn schema(&self) -> &Schema {
+        &self.schema
+    }
+    fn children(&self) -> Option<&PhysicalPlan> {
+        todo!()
+    }
+    fn execute(self) -> Vec<RecordBatch> {
+        todo!()
+    }
 }
